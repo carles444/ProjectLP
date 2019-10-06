27f7f718dcdd75502de68c5263486e984c68b994
45Hack45 <45haha45@gmail.com>
2019-10-06 17:17:38 +0200

    test

--- Projecte/MatriuDispersa.h
+++ Projecte/MatriuDispersa.h
@@ -6,10 +6,6 @@ public:
 private:
 	int* fila;
 	int* columna;
-
-	//L'enunciat dui que ho posem pero jo crec que es inutil
-	//perque nomes guardem els que tenen valor 1
-	//per tant valor tindra 1 en totes les posicions
 	int* valor;
 };
 
